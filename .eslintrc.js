cd76c37dac67ef309ca59cbf0689c7c0e6845e95
Aaron Gong <aaronjxz@gmail.com>
2019-02-28 11:25:01 +0800

    initial commit, awaiting decision on UI

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    // 'plugin:vue/essential',
+    '@nuxtjs',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    // 'vue',
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': 'off'
+  }
+}
