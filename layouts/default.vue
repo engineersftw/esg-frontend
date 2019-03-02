ddb23af12ee82756659aa0ac5420f0118e27cd79
Aaron Gong <aaronjxz@gmail.com>
2019-03-02 17:15:33 +0800

    dashboard skeleton somewhat for now

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,7 +106,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 a {
   color: white;
   text-decoration: none;
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,44 @@
 <template>
   <v-content>
     <v-container fill-height>
+      <!-- <v-layout row wrap>
+      </v-layout> -->
       <v-layout row wrap>
         <v-flex xs12>
           <h1>Welcome To Engineers.sg</h1>
-          <h1>Latest Videos</h1>
         </v-flex>
-        <v-flex md8 sm12 class="px-1">
-          <latest-video-item />
+        <v-flex md8 sm12>
+          <h2>Latest Videos</h2>
+          <v-layout row wrap>
+            <v-flex xs12 class="pa-1">
+              <latest-video-item />
+            </v-flex>
+            <v-flex xs12 class="pa-1">
+              <latest-video-item />
+            </v-flex>
+          </v-layout>
         </v-flex>
-        <v-flex md4 sm12 class="px-1">
-          <h1>Support</h1>
-
-          <a
-            class="sc-hMqMXs bQQHMC"
-            color="primary"
-            tabindex="0" type="button"
-            href="https://www.patreon.com/bePatron?u=752396&amp;redirect_uri=https%3A%2F%2Fengineers.sg%2F&amp;utm_medium=widget"
-            role="button"
-            target="_blank"
-          >
-            <div class="sc-cSHVUG bDCJxk" display="flex">
-              <div class="sc-ckVGcZ hnTNrw">
-                <span class="sc-dxgOiQ ZmzLr">
-                  <svg viewBox="0 0 569 546" xmlns="http://www.w3.org/2000/svg">
-                  <g>
-                    <circle data-fill="1" id="Oval" cx="362.589996" cy="204.589996" r="204.589996"></circle>
-                    <rect data-fill="2" id="Rectangle" x="0" y="0" width="100" height="545.799988"></rect>
-                  </g>
-                  </svg>
-                </span>
-              </div>
-            <div class="sc-hSdWYo gBDeVU">
-            </div>
-            <div class="button-text">Become a patron</div>
-            </div>
-          </a>
-          <h1>Events</h1>
-          <p>WeBuild.SG is a list of free open events and open source libraries for the curious folks who love to make things!</p>
-          <latest-event-item />
+        <v-flex md4 sm12>
+          <h2>Support</h2>
+          <v-btn dark color="#f96854" href="https://www.patreon.com/bePatron?u=752396&redirect_uri=https%3A%2F%2Fengineers.sg%2F&utm_medium=widget" target="_blank">
+            <v-avatar size="24px">
+              <img src="/patreon-dark.png">
+            </v-avatar>
+            Become A Patreon
+          </v-btn>
+          <h2>Events</h2>
+          <p><a class="we-build" href="https://webuild.sg" target="_blank">WeBuild.SG</a> is a list of free open events and open source libraries for the curious folks who love to make things!</p>
+          <v-layout row wrap>
+            <v-flex xs12 class="pa-1">
+              <latest-event-item />
+            </v-flex>
+            <v-flex xs12 class="pa-1">
+              <latest-event-item />
+            </v-flex>
+            <v-flex xs12 class="pa-1">
+              <latest-event-item />
+            </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
@@ -57,34 +57,8 @@ export default {
 }
 </script>
 
-<style>
-/* .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+a.we-build {
+  color: #0088ff;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-} */
 </style>
